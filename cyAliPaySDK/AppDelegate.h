3aeef32e7a262515a2e963dc9b2498504c291050
叶子 <yeyifeng@yeyifengdeMacBook-Pro.local>
2018-02-27 11:11:49 +0800

    Initial Commit

---
+++ cyAliPaySDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cyAliPaySDK
+//
+//  Created by 叶子 on 2018/2/27.
+//  Copyright © 2018年 叶子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cyAliPaySDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cyAliPaySDK
+//
+//  Created by 叶子 on 2018/2/27.
+//  Copyright © 2018年 叶子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
